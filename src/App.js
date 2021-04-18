de1734b638f11078b86c1c1817ca2810eb5a0d7f
Abdoul_Master0110 <abdoulafoudal@gmail.com>
2021-04-18 00:00:12 +0000

    site vitrine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import React from 'react';
+import Home from './components/pages/Home';
+import Navigationbar from './components/Navigationbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Services from './components/pages/Services';
+import AboutUs from './components/pages/AboutUs';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <h1>HI</h1>
-    </div>
+    <>
+      <Router>
+        <Navigationbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/aboutUs' component={AboutUs} />
+          <Route path='/sign-up' component={SignUp} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
