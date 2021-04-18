8868548b9a6771b48862d977968add163d081390
Abdoul_Master0110 <abdoulafoudal@gmail.com>
2021-04-18 23:18:06 +0000

    modifications 1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -64,11 +64,11 @@ function Footer() {
         <div class='social-media-wrap'>
           <div class='footer-logo'>
             <Link to='/' className='social-logo'>
-              TRVL
-              <i class='fab fa-typo3' />
+            A$H
+            <i class="fab fa-superpowers" aria-hidden="true"></i>
             </Link>
           </div>
-          <small class='website-rights'>TRVL © 2020</small>
+          <small class='website-rights'>TRVL © 2021</small>
           <div class='social-icons'>
             <Link
               class='social-icon-link facebook'
